e90c9dcd44cb74394c49fe962ea03c29168eed5c
Anurag Krishna <KH2567@kitspl.com>
2019-07-02 12:56:08 +0530

    geolocation sample

---
+++ controllers/desktop/frmParentController/frmParentControllerActions.js
@@ -0,0 +1,10 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for Button0j9a8f37a30934c **/
+    AS_Button_c8f877c2438b42ff9f46adde897aaba8: function AS_Button_c8f877c2438b42ff9f46adde897aaba8(eventobject) {
+        var self = this;
+        return self.getLocation.call(this);
+    }
+});
